268fed16d044e57309de598601b849930955b619
Palak <palaktyagi000@gmail.com>
2021-08-07 23:37:41 +0530

    Day 17

---
+++ 10. 2D arrays/checkin.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+	vector<int> ans;
+	int i =0;
+	if(i==0){
+		return ans;
+	}
+}
---
+++ 20.Bitwise operators/Bit-manipulation.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+bool isOdd(int n){
+	return(n&1);
+}
+
+
+
+int getBit(int n, int i){
+	int mask = (1<<i);
+	int bit = (n & mask) > 0 ? 1 : 0;
+	return bit;
+}
+
+int setBit(int n, int i){
+	int mask = (1 << i);
+	int ans = n | mask;
+	return ans;
+}
+
+void clearBit(int &n, int i){
+	int mask = ~(1<<i);
+	n = n & mask;
+}
+
+void updateBit(int &n, int i, int v){
+	int mask = ~(1<<i);
+	int cleared_bit_no = n & mask;
+	n = cleared_bit_no | (v<<i); 
+}
+
+int clearLastBits(int n, int i){
+	int mask = (-1<<i);
+	return n & mask;
+}
+
+int clearRangeItoJ(int n, int i, int j){
+	int ones = (~0);
+	int a = ones<<(j+1);
+	int b = (1<<i)-1;
+	int mask = a | b;
+	int ans = n & mask;
+	return ans;
+}
+
+
+
+int main(){
+
+	int n = 31;
+	int i = 1, j=3; //from position i+1 to 0
+	//cin>>i;
+	//cout<<clearLastBits(n,i)<<endl;
+	cout<<clearRangeItoJ(n,i,j)<<endl;
+	
+    /*cout<<isOdd<<n<<endl;
+    cout<<getBit(n,i)<<endl;
+    n= setBit(n, i);
+    cout<<"Ans "<<n<<endl;
+    
+    clearBit(n,i);
+    cout<<n<<endl;
+
+    updateBit(n, 2, 0);
+    updateBit(n, 3, 1);
+    cout<<n<<endl;
+*/
+
+	return 0;
+}
